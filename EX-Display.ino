f1855a59c4899ed96bd4ad4ccc97dd89f6c03004
vacumcollapse <matt@vacuumcollapse.com>
2024-04-06 22:26:03 +0100

    First working version - no apologies for quality

--- EX-Display.ino
+++ EX-Display.ino
@@ -2,7 +2,6 @@
  *  © 2024, Matt Bidwell, Paul Antoine, Colin Murdoch, Chris Harlow
  *  All rights reserved.
  *
- *  This file is part of CommandStation-EX
  *
  *  This is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
