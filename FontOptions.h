8ba6494f0d6dd0482e348aa64a29e5b1eb13497f
peteGSX <peteracole@outlook.com.au>
2024-05-06 08:13:50 +1000

    Include fonts

--- FontOptions.h
+++ FontOptions.h
@@ -1,11 +1,8 @@
 #ifndef FONTOPTIONS_H
 #define FONTOPTIONS_H
 
-// #include "Arial9pt7b.h"
-// #include "FreeSans12pt7b.h"
-// #include <Adafruit_GFX.h>
-// #include <Fonts/FreeSans9pt7b.h>
-// #include <Fonts/GFXFF/FreeMono9pt7b.h>
+#include "Arial9pt7b.h"
+#include "FreeSans12pt7b.h"
 
 /// @brief Macros for available colours
 #define BLACK 0x0000
