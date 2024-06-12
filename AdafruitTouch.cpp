75284168b99cb2a381c418358807e1ee615e5078
Colin Murdoch <colin.murdoch@btinternet.com>
2024-06-12 20:34:24 +0100

    Change to map definitions
    Change to map definitions to allow compile,

--- AdafruitTouch.cpp
+++ AdafruitTouch.cpp
@@ -16,8 +16,8 @@ void AdafruitTouch::begin() {
 bool AdafruitTouch::_readRawInput(ButtonName button) {
   TSPoint tsPoint = _touchScreen.getPoint();
   if (tsPoint.z > _touchScreen.pressureThreshhold) {
-    uint16_t x = map(tsPoint.x, TS_MINX, TS_MAXX, 0, _screen->getWidth());
-    uint16_t y = map(tsPoint.y, TS_MINY, TS_MAXY, 0, _screen->getHeight());
+    uint16_t x = map(tsPoint.x, TS_LEFT, TS_RT, 0, _screen->getWidth());
+    uint16_t y = map(tsPoint.y, TS_TOP, TS_BOT, 0, _screen->getHeight());
     if (x >= _buttons[button].xStart && x <= _buttons[button].xEnd && y >= _buttons[button].yStart &&
         y <= _buttons[button].yEnd) {
       return true;
